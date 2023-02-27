840a753e889a20d8af47b901dcba93b523b15e8b
odid2730 <odid2730@gmail.com>
2023-02-27 22:20:17 +0900

    ATL tutorials part 1

---
+++ Polygon/Polygon/dllmain.h
@@ -0,0 +1,10 @@
+﻿// dllmain.h : 모듈 클래스의 선언입니다.
+
+class CPolygonModule : public ATL::CAtlDllModuleT< CPolygonModule >
+{
+public :
+	DECLARE_LIBID(LIBID_PolygonLib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_POLYGON, "{373be9d1-7488-42cc-b776-451592ba7cd6}")
+};
+
+extern class CPolygonModule _AtlModule;
